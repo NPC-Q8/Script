f27d9c9ca9c2cf87fb226b2251c4dd24fd32da10
NPC-Q8 <160725407+NPC-Q8@users.noreply.github.com>
2024-05-22 19:04:54 +0700

    Add files via upload

---
+++ idk4.lua
@@ -0,0 +1,13 @@
+--Check Material
+function CheckMaterial(matname)
+for i,v in pairs(game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("getInventory")) do
+if type(v) == "table" then
+if v.Type == "Material" then
+if v.Name == matname then
+return v.Count
+end
+end
+end
+end
+return 0
+end
